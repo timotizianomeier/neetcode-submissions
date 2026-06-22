1ff9734d3828adc4ec28706f7ff9551bd7061da3
Timo Meier <121181597+timotizianomeier@users.noreply.github.com>
2026-06-22 10:44:06 +0100

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # HashMap ith character counts for key, O(m*n)
+        hashMap = {}
+        for i, txt in enumerate(strs):
+            counts = [0] * 26
+            for ch in txt:
+                counts[ord(ch) - ord('a')] = counts[ord(ch) - ord('a')] + 1
+            if hashMap.get(tuple(counts), -1) != -1:
+                hashMap[tuple(counts)].append(txt)
+            else:
+                hashMap[tuple(counts)] = [txt]
+        return list(hashMap.values())
+
+        # initial attempt, O(m*n*log(n))
+        hashMap = {}
+        for i, txt in enumerate(strs):
+            if hashMap.get(''.join(sorted(txt)), -1) == -1:
+                hashMap[''.join(sorted(txt))] = [txt]
+            else:
+                hashMap[''.join(sorted(txt))].append(txt)
+        return list(hashMap.values())
